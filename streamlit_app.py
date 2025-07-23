145ffa5dc388360d6816191251d2d57509c4dcb2
abeisov <109777504+abeisov@users.noreply.github.com>
2025-07-23 19:06:24 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,14 @@ st.write("## Работа с датасетом пингвинов")
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
 st.subheader("🔹 Случайные 10 строк")
 st.dataframe(df.sample(10), use_container_width=True)
+st.subheader("📊 Визуализация данных")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    fig1 = px.histogram(df, x="species", color="island", barmode="group", title="Распределение видов по островам")
+    st.plotly_chart(fig1, use_container_width=True)
+
+with col2:
+    fig2 = px.scatter(df, x="bill_length_mm", y="flipper_length_mm", color="species", title="Длина клюва vs Длина крыла")
+    st.plotly_chart(fig2, use_container_width=True)
