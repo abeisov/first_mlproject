c07802d00103aeeaa46d06316e8a2788d0e538ac
abeisov <109777504+abeisov@users.noreply.github.com>
2025-07-23 18:43:42 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import category_encoders as ce
 
-st.title('🎈 App Name')
+st.set_page_config(page_title="🐧 Penguin Classifier", layout="wide")
+st.title('🐧 Penguin Classifier – Обучение и предсказание')
+st.write("## Работа с датасетом пингвинов")
 
-st.write('Hi user!')
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
